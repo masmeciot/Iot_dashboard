f59a888d544ca09d57b2169c2c31caa442f378f2
Senith Deelaka Ranchagoda <maskrdmec@gmail.com>
2025-07-18 08:52:23 +0530

    Update IoT and Synergy menu views with new titles and button links; modify project file to include additional views and package references.

---
+++ Controllers/Iot/iotmainmenu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Iot_dashboard.Controllers.Iot
+{
+    public class iotmainmenu : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/iotmainmenu.cshtml");
+        }
+    }
+}
